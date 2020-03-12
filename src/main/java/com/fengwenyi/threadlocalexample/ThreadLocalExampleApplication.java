28409758ba65defaadd8359456bec387e148dded
e <xfsy_2015@163.com>
2020-03-12 12:16:30 +0800

    init

---
+++ src/main/java/com/fengwenyi/threadlocalexample/ThreadLocalExampleApplication.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.threadlocalexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadLocalExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadLocalExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengwenyi/threadlocalexample/ThreadLocalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.threadlocalexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadLocalExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
